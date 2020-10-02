a8e9dbe93407706ace881e91fc48ecad538e645f
Connor Zanin <czanin@newstore.com>
2020-10-02 15:31:24 -0600

    Added debug endpoint

--- main.go
+++ main.go
@@ -9,5 +9,10 @@ import (
 func main() {
 	http.HandleFunc("/event", EventHandler)
 	http.HandleFunc("/authorize", AuthorizeHandler)
+	http.HandleFunc("/test", TestHandler)
 	log.Fatal(http.ListenAndServeTLS(":443", os.Getenv("PEMFILE"), os.Getenv("KEYFILE"), nil))
+}
+
+func TestHandler(w http.ResponseWriter, req *http.Request) {
+	w.Write([]byte("Response from /test"))
 }
